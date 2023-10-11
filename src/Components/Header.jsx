67b6327412f000e3871b6b4bdd655130fd71f1ff
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-10-12 00:40:29 +0530

    removed console logs

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -15,11 +15,6 @@ const Header = ({ onLatLngChanged }) => {
         <Autocomplete
           apiKey="AIzaSyCNnXEb2XOFlTcwMcBYK31-6x05FY0oUPc"
           onPlaceSelected={(place) => {
-            console.log(place);
-            console.log(place.geometry.location.lat());
-            console.log(place.geometry.location.lng());
-            // setLat(place.geometry.location.lat());
-            // setLng(place.geometry.location.lng());
             let location = {
               lat: place.geometry.location.lat(),
               lng: place.geometry.location.lng(),
